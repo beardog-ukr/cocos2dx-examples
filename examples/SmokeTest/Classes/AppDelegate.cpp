cf53ba0b3fc1d3a7b1602dc5b38223a1c71c249a
Your Name <you@example.com>
2020-03-23 22:24:46 +0200

    cleaned code, removed unused design resoultion calculation

--- examples/SmokeTest/Classes/AppDelegate.cpp
+++ examples/SmokeTest/Classes/AppDelegate.cpp
@@ -11,9 +11,6 @@ using namespace cocos2d::experimental;
 USING_NS_CC;
 
 static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-// static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-// static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-// static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
 AppDelegate::AppDelegate() {
 }
@@ -65,22 +62,6 @@ bool AppDelegate::applicationDidFinishLaunching() {
   // Set the design resolution
   glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height,
                                   ResolutionPolicy::NO_BORDER);
-  // auto frameSize = glview->getFrameSize();
-  // // if the frame's height is larger than the height of medium size.
-  // if (frameSize.height > mediumResolutionSize.height) {
-  //   director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height,
-  //                                       largeResolutionSize.width/designResolutionSize.width));
-  // }
-  // // if the frame's height is larger than the height of small size.
-  // else if (frameSize.height > smallResolutionSize.height) {
-  //   director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height,
-  //                                       mediumResolutionSize.width/designResolutionSize.width));
-  // }
-  // // if the frame's height is smaller than the height of medium size.
-  // else {
-  //   director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height,
-  //                                       smallResolutionSize.width/designResolutionSize.width));
-  // }
 
   register_all_packages();
 
